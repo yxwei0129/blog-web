a17231061d258ae5036e4cfcf21ec8bcb6b640df
yxwei0129 <745780509@qq.com>
2019-07-21 16:48:13 +0800

    token测试

--- src/pages/managementCenter/managementCenter.js
+++ src/pages/managementCenter/managementCenter.js
@@ -6,15 +6,11 @@ import router from './router/index'
 import store from './store/index'
 import Header from './components/Header'
 import axios from './axios/index'
-import { Message } from 'element-ui'
-
-// Vue.use(Message)
-Vue.component(Message)
-Vue.prototype.$message = Message
-
 Vue.prototype.HOST = '/api'
 Vue.prototype.$store = store
 window.$axios = axios
+window.globalStore = store
+window.globalRouter = router
 router.beforeEach((to, from, next) => {
   // 会在任意路由跳转前执行，next一定要记着执行，不然路由不能跳转了
   // eslint-disable-next-line
@@ -28,5 +24,14 @@ export default new Vue({
   store,
   components: {
     'top-header': Header
+  },
+  created () {
+    // 在页面加载时读取localStorage里的状态信息
+    sessionStorage.getItem('userMsg') && this.$store.replaceState(Object.assign(this.$store.state, JSON.parse(sessionStorage.getItem('userMsg'))))
+
+    // 在页面刷新时将vuex里的信息保存到localStorage里
+    window.addEventListener('beforeunload', () => {
+      sessionStorage.setItem('userMsg', JSON.stringify(this.$store.state))
+    })
   }
 })
