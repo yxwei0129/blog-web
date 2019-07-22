09eb790d68e6ee512c1c982f73dfcd1a096a5ad2
yxwei0129 <745780509@qq.com>
2019-07-22 23:46:27 +0800

    添加常量类

---
+++ src/constants.js
@@ -0,0 +1,14 @@
+/**
+ * created by Yuxin Wei on 2019/7/22
+ * 常量封装
+ */
+
+export default {
+  responseCode: {
+    login: {
+      success: 'SH-1003',
+      empty: 'SH-1002',
+      error: 'SH-1001'
+    }
+  }
+}
