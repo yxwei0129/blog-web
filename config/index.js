4d1f8f2b23387123c0bd5f16a507a14385fa38b4
yxwei0129 <745780509@qq.com>
2019-07-23 22:12:10 +0800

    webpack-dev-server 代理

--- config/index.js
+++ config/index.js
@@ -10,7 +10,16 @@ module.exports = {
     // Paths
     assetsSubDirectory: 'static',
     assetsPublicPath: '/',
-    proxyTable: {},
+    proxyTable: {
+      '/management': {    //将www.exaple.com印射为/apis
+        target: 'http://localhost:3000',  // 接口域名
+        secure: false,  // 如果是https接口，需要配置这个参数
+        changeOrigin: true,  //是否跨域
+        pathRewrite: {
+          '^/management': '/management'   //需要rewrite的,
+        }
+      }
+    },
 
     // Various Dev Server settings
     host: 'localhost', // can be overwritten by process.env.HOST
