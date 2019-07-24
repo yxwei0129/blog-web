da281bdcc0a485d5e228818337470ed3ce269f7a
yxwei0129 <745780509@qq.com>
2019-07-24 22:59:53 +0800

    全屏插件

--- static/js/wangEditor-fullscreen-plugin.js
+++ static/js/wangEditor-fullscreen-plugin.js
@@ -4,7 +4,7 @@
 window.fullscreen = {
   // editor create之后调用
   init: function (editorSelector) {
-    $(editorSelector + ' .w-e-toolbar').append('<div class="w-e-menu"><a class="_wangEditor_btn_fullscreen" href="###" onclick="window.fullscreen.toggleFullscreen(\'' + editorSelector + '\')">全屏</a></div>')
+    $(editorSelector + ' .w-e-toolbar').append('<div class="w-e-menu" style="position: absolute;right: 20px"><a class="_wangEditor_btn_fullscreen" href="###" onclick="window.fullscreen.toggleFullscreen(\'' + editorSelector + '\')">全屏</a></div>')
   },
   toggleFullscreen: function (editorSelector) {
     $(editorSelector).toggleClass('fullscreen-editor')
